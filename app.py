82ef3db6c340cfa1940aa3c3ce70b90faf48f9ae
DhruvGoyal375 <dhruvgoyal375@gmail.com>
2023-07-10 22:16:57 +0530

    change company_name

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ JOBS = [
 ]
 @app.route("/")
 def hello_world():
-    return render_template("home.html", jobs=JOBS, company="Jovian")
+    return render_template("home.html", jobs=JOBS, company_name="Jovian")
 
 @app.route('/api/jobs')
 def list_jobs():
