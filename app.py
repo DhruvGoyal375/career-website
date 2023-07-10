eb1027cf6542eb88279d360334f3521246c0ff9b
DhruvGoyal375 <dhruvgoyal375@gmail.com>
2023-07-10 21:53:03 +0530

    Add Tailwind

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+JOBS = [
+    {
+        "id":1,
+        "title": "data analyst",
+        "location": "Jgd",
+        "salary":"30000"
+    }, 
+    {
+        "id":2,
+        "title": "eng",
+        "location": "remote",
+        "salary":"3000"
+    }, 
+]
+@app.route("/")
+def hello_world():
+    return render_template("home.html", jobs=JOBS)
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
